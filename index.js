977efad33bce2028750c16100a10d64bbb4393e2
vedantpawar18 <vedantpawar18@gmail.com>
2022-09-05 17:12:10 +0530

    added

--- index.js
+++ index.js
@@ -7,18 +7,20 @@ const dns = require("dns");
 app.post('/getmeip',function(req, res) {
     const payload=(req.body)
     const website=payload.website_name;
+    console.log(website)
     dns.resolve4(website, (err, addresses) => {
         if (err) {
           console.err(err);
           return;
         }
-        res.send(addresses[0]); 
+        res.send(addresses[0])
       });
       const data= fs.readFileSync("./db.json", {encoding:"utf-8"})
       const parseData=JSON.parse(data);
        const websites= parseData.websites
       const Newwebsites=[...websites, payload];
-      const latest_data= JSON.stringify(Newwebsites)
+      parseData.websites=Newwebsites;
+      const latest_data= JSON.stringify(parseData)
       fs.writeFileSync("./db.json", latest_data, "utf-8")
       console.log(Newwebsites)
 });
@@ -27,11 +29,13 @@ app.post("/products/create", (req,res)=>{
     const payload=(req.body)
     const data= fs.readFileSync("./products.json", {encoding:"utf-8"})
     const parseData=JSON.parse(data);
-    const newProduct=[...parseData.products, payload]
-    console.log(newProduct)
-    const latest_data= JSON.stringify(newProduct)
+     const products= parseData.products
+    const Newproducts=[...products, payload];
+    parseData.products=Newproducts;
+    const latest_data= JSON.stringify(parseData)
     fs.writeFileSync("./products.json", latest_data, "utf-8")
-    res.send("product added") 
+    console.log(Newproducts)
+    res.send("product added")
 
 })
 
@@ -45,10 +49,25 @@ app.get("/products", (req,res)=>{
 app.put("/products", (req,res)=>{
     const data=fs.readFileSync("./products.json", {encoding:"utf-8"})
     const parsedData = JSON.parse(data)
-    const product= parsedData.find(item => item.id === req.body.id);
-    const index = parsedData.indexOf(product);
+    const products= parsedData.products
+    const product= products.find(item => item.id === req.body.id);
+    const index = products.indexOf(product);
     console.log(index)
-    parsedData[index] = req.body;
+    products[index] = req.body;
+    const latest_data= JSON.stringify(parsedData)
+    fs.writeFileSync("./products.json", latest_data, "utf-8")
+    res.send("product updated")
+})
+
+app.delete("/products", (req,res)=>{
+    const data=fs.readFileSync("./products.json", {encoding:"utf-8"})
+    const parsedData = JSON.parse(data)
+    const products= parsedData.products
+    const product= products.find(item => item.id === req.body.id);
+    const index = products.indexOf(product);
+    console.log(index)
+    products.splice(index, 1);
+    // products[index] = req.body;
     const latest_data= JSON.stringify(parsedData)
     fs.writeFileSync("./products.json", latest_data, "utf-8")
     res.send("product updated")
